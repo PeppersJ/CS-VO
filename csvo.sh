ee4770dd465413aae04355ba0ef3d24737e6ff60
PepeprsJ <52257517+PeppersJ@users.noreply.github.com>
2020-07-15 18:52:58 -0700

    Added world file to compile

--- csvo.sh
+++ csvo.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-g++ CS-VO.cpp queue.cpp collison.cpp entities.cpp charMap.cpp -g -std=c++17 -lncurses
+g++ CS-VO.cpp queue.cpp collison.cpp entities.cpp charMap.cpp world.cpp -g -std=c++17 -lncurses
